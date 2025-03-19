3e5bcab333080999cee53e38ee5cd0fe7e68ad97
IsaacVNX <isaacmoura.c69@gmail.com>
2025-03-19 19:37:55 -0300

    Api Livros Vai Na Web

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import sqlite3  # Biblioteca para interagir com o banco de dados SQLite
-from flask import Flask, request, jsonify  # Importamos Flask para criar a API e request/jsonify para manipular requisições e respostas
+import sqlite3 
+from flask import Flask, request, jsonify  
 
 app = Flask(__name__)
 
