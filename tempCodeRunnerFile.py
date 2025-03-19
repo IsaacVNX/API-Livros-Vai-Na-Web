f35f6da01e6d464df4def4a5b9d9805f7ca3babe
IsaacVNX <isaacmoura.c69@gmail.com>
2025-03-19 18:25:46 -0300

    Api Livros Vai Na Web

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+def init_db():
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(
+            """
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,  
+                    titulo TEXT NOT NULL,  
+                    categoria TEXT NOT NULL,  
+                    autor TEXT NOT NULL,  
+                    image_url TEXT NOT NULL  
+                )
+            """
+        ) 
+
+init_db()
